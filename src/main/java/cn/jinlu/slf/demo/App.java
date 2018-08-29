cca0b3c8f00db85810ec7e75230f3d1bbf116749
lu02.jin <lu02.jin@vipshop.com>
2018-08-29 16:06:20 +0800

    init

---
+++ src/main/java/cn/jinlu/slf/demo/App.java
@@ -0,0 +1,14 @@
+package cn.jinlu.slf.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App
+{
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main( String[] args )
+    {
+        logger.info( "Hello, {}!", App.class.getSimpleName());
+    }
+}
